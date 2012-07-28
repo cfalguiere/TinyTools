832f35eae7f222fdf59f4804c2127685b3d9afea
Claude Falguiere <cfalguiere@gmail.com>
2012-07-28 16:06:45 +0200

    file is now removed after diff and fixed source presence test

--- music/lastfm/filter-new.bash
+++ music/lastfm/filter-new.bash
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 #TODO all file when no file in repo
-#TODO ivy can't read the file (invalid characters)
 
 REPO=./repo
+DL=~/Downloads
 
 mkdir -p ${REPO}
 
 for TAG in friends recos wish_list
 do
+     echo "Scanning file for ${TAG}"
 
      TAG_FILE=niftypuce_tag_${TAG}
+     SOURCE_FILE=${DL}/niftypuce_tag_${TAG}.tsv
 
-     if [ -f ${TAG_FILE}.tsv ]  
+     if [ -f ${SOURCE_FILE} ]  
      then 
+	  echo "Found ${TAG_FILE}"
+     	  cp -f ${SOURCE_FILE} ${REPO}
 
-     	cp -f ~/Downloads/${TAG_FILE}.tsv ${REPO}
-
-    	 CURRENT_FILE=${REPO}/${TAG_FILE}.tsv 
-     	 OLD_FILE=${REPO}/${TAG_FILE}.csv 
+    	  CURRENT_FILE=${REPO}/${TAG_FILE}.tsv 
+     	  OLD_FILE=${REPO}/${TAG_FILE}.csv 
     	  NEW_FILE=${TAG_FILE}_new.csv 
 
-     	  comm -23 <(sort ${CURRENT_FILE}) <(sort ${OLD_FILE}) > ${NEW_FILE}
+	  head -1 ${CURRENT_FILE} > ${NEW_FILE}
+     	  comm -23 <(sort ${CURRENT_FILE}) <(sort ${OLD_FILE}) >> ${NEW_FILE}
 
      	  find . -size 0 -name "*.csv" -exec rm -f '{}' \;  #remove empty files
 
      	  mv ${CURRENT_FILE} ${OLD_FILE}
+     	  rm ${SOURCE_FILE} 
      
      fi 
 
